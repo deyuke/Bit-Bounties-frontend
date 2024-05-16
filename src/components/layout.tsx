ec60fc32fd23462f9b7b83473ec78f1753c46943
?YUSUF KEHYA <devrankehya@hotmail.com>
2024-05-16 13:29:20 +0200

    refactor: Update import statement for Navbar component in layout.tsx

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,4 +1,4 @@
-import Navbar from "./organisms/Navbar";
+import { Navbar } from "./organisms/_index";
 
 // Path: bit-bounties/src/components/layout.tsx
 const Layout = ({ children }: { children: React.ReactNode }) => {
