281bec8a5cf29df67533b5f83b011427646bc779
?YUSUF KEHYA <devrankehya@hotmail.com>
2024-05-16 13:35:11 +0200

    feat: Update homepage with new HeroSection and HomepageStepDiscover components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,43 +1,18 @@
-import Image from "next/image";
+import { HeroSection, HomepageStepDiscover } from "@/components/organisms/_index";
+
 export default function Home() {
     return (
-        <main className="flex justify-center">
-            <div className="max-w-7xl">
-              <div className="flex justify-between">
-                <div className="flex flex-col gap-[40px] w-[750px]">
-                    <Image
-                        src="/Bit-Bounties-logo.svg"
-                        alt="Bit Bounties Logo"
-                        width={466}
-                        height={108}
-                    />
-                    <div className="flex flex-col gap-4">
-                      <div className="flex flex-col gap-2">
-                        <h1 className="font-[50px]">
-                            Welcome to Bit Bounties - Where Your Skills Meet
-                            Opportunities!
-                        </h1>
-                        <p className="text-[32px] font-normal text-secondary">
-                            Are you tired of the same old job application
-                            routine? Bit Bounties is here to transform your job
-                            search experience!
-                        </p>
-                      </div>
-                        <div className="flex gap-3 text-2xl">
-                            <button className="bg-primary hover:bg-primary/85 hover:shadow-primary/30 hover:shadow-md text-black py-3 px-[18px] rounded-lg ">
-                                Start hunting
-                            </button>
-                            <button className="hover:bg-primary/10 text-primary py-3 px-[18px] rounded-lg">
-                                How it works
-                            </button>
-                        </div>
+        <main className="flex justify-center items-center pt-8 lg:pt-36 px-3">
+            <div className="max-w-7xl w-full">
+                <div>
+                    <HeroSection />
+                    <div className="mt-32 flex flex-col gap-24">
+                        <HomepageStepDiscover />
                     </div>
                 </div>
-                <div >
-                    <Image src={"/img/hero2.jpg"} layout="fill" alt="A student who used BitBounties" width={512} height={512}/>
-                </div>
-              </div>
             </div>
         </main>
     );
 }
+
+// width={512} height={512}
