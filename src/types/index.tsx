2708e8a1cbd5dddd10347a4ef0386438790c2adc
?YUSUF KEHYA <devrankehya@hotmail.com>
2024-05-29 15:20:50 +0200

    feat: Add types for Bounty and Company
    Added new types for Bounty and Company in the index.tsx file under the types directory.

---
+++ src/types/index.tsx
@@ -0,0 +1,25 @@
+export type Bounty = {
+    key?: number;
+    id: number;
+    company_id: number;
+    title: string;
+    slug: string;
+    description: string;
+    is_active: number;
+    created_at: string;
+    updated_at: string;
+}
+
+export type Company = {
+    id: number;
+    name: string;
+    email: string;
+    phone: string;
+    website: string;
+    street: string;
+    postal_code: string;
+    city: string;
+    country: string;
+    created_at: string;
+    updated_at: string;
+}
