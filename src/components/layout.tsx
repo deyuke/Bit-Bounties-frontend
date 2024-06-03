9fa6591668caa7390cb9f65b769befd77d76e2d7
?YUSUF KEHYA <devrankehya@hotmail.com>
2024-06-03 13:26:35 +0200

    feat: Add Footer component in layout component

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,11 +1,13 @@
-import { Navbar } from "./organisms/_index";
+import React from "react";
+import { Navbar, Footer } from "./organisms/_index";
 
-// Path: bit-bounties/src/components/layout.tsx
 const Layout = ({ children }: { children: React.ReactNode }) => {
     return (
         <>
+            <div className="bg-black bg-primary hidden" />
             <Navbar />
             {children}
+            <Footer />
         </>
     );
 };
